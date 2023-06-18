00159386621c3f6029864f478fc596a02f24ec0f
cmu-samuelch <samuelch@andrew.cmu.edu>
2023-06-18 16:36:13 -0500

    command line argument support added for send_msg

--- send_msg.py
+++ send_msg.py
@@ -1,9 +1,37 @@
-import discord, botData
+import discord
+import botData
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = '.')
 
-msg = "(you know that talk is cheap)"
+def parse_args(argv):
+    argv.pop(0)
+    channel, msg, reply = None, None, None
+    while argv:
+        curr_arg = argv.pop(0)
+        if not argv: return (None, None, None)
+        if curr_arg == "-c":
+            channel = get_channel(argv.pop(0))
+        elif curr_arg == "-m":
+            msg = argv.pop(0)
+        elif curr_arg == "-r":
+            reply = find_msg(argv.pop(0))
+        else:
+            return (None, None, None)
+    return (channel, msg, reply)
+
+def get_channel(path, bot):
+    return 1014608064646746112 if path == "channel5" else 999415862069039144
+    if path.isdigit():
+        return bot.get_channel(int(path))
+    else:
+        separator_index = path.find('/')
+        server = path[:separator_index]
+        channel = path[separator_index + 1:]
+        return
+
+def find_msg(id):
+    pass
 
 @bot.event
 async def on_ready():
@@ -16,4 +44,10 @@ async def on_ready():
 
     await bot.close()
 
-bot.run(botData.token)
+(channel, msg, reply) = None, None, None
+def main():
+    (channel, msg, reply) = parse_args(sys.argv)
+    if None in (channel, msg): return
+    
+    print((channel, msg, reply))
+#    bot.run(botData.token)
